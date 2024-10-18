c651ee19819b9ea6c6d62b190dae2467e402ce54
Vijay <vijay333kandhasamy@gmail.com>
2024-10-18 13:59:55 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:provider/provider.dart';
 import 'package:wereal_v2/features/auth/service/auth_service.dart';
 import 'package:wereal_v2/features/roadmap/service/roadmap_service.dart';
 import 'package:wereal_v2/utils/routes/routes.dart';
-
+import 'package:flutter_no_internet_widget/flutter_no_internet_widget.dart';
 import 'features/roadmap/view/roadmap/questions_page.dart';
 import 'firebase_options.dart';
 
@@ -25,11 +25,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var val = Provider.of<AuthenticationProvider>(context,listen: false).checkSignin();
-    return MaterialApp(
+    return InternetWidget(
+      offline: const FullScreenWidget(
+        child: Scaffold(
+          body: Center(
+            child: Text('No internet connection'),
+          ),
+        ),
+      ),
+      loadingWidget: const Center(child: Text('Loading')),
+      online: MaterialApp(
+        home: MaterialApp(
       title: 'Wereal V2',
       debugShowCheckedModeBanner: false,
       initialRoute: val ? '/home' : '/sign-in',
       routes: AppRoutes.routes,
-    );
+    ),
+      ),
+    );;
   }
 }
--- lib/utils/constants/consts.dart
+++ lib/utils/constants/consts.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class API {
-  static String BASE_URL = "https://2ef3-2409-40f4-3017-f674-4d66-23c9-5d72-4d9c.ngrok-free.app";
+  static String BASE_URL = "https://bright-glowworm-correctly.ngrok-free.app/";
   static String GET_ROAD_MAP = "/api/ai/genrate";
 
 }
